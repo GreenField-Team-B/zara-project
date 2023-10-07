6dcd3b8db71936a283447f8af9dc8f8a163db46c
rezsirine <sirinerezgui585@gmail>
2023-10-07 16:50:20 +0100

    product done

--- client/src/components/OneProduct.jsx
+++ client/src/components/OneProduct.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import  heartImage from './images/heart.png'
+import  groupImage from './images/Group.png'
 import "./oneProduct.css";
 import moment from "moment";
 const OneProduct = ({ product }) => {
@@ -14,8 +16,8 @@ const OneProduct = ({ product }) => {
           src="https://s3-alpha-sig.figma.com/img/04a1/915f/d6cedd7c8b1073685c5f1be1b50e1ac6?Expires=1697414400&Signature=cHgvjZ3w5dwEDTk38l11QBqMYrD5eYG1K3AXU5vbSTV0LqDOLHeL5bahveT61CDmnZ-VBpmN7IS2VdFBvwn0hbptVK3BTI6vj6LOe~gY20MpdrxwixYcCMEzGOsWLzwFdLgTSiDwar-ZNepiQPbPE0nE8zwkVSvhSoCPwZ-Rl8g10t~9e8GmABRGLENyfBMpdN~2YeaOsfw4o2xfkHBx8Wnnoib2IVCzHmHBQiTqCBLjj8OQ5gZ~EVAOzem7BuxVyCqCufKrdDAV~ZbPRwCiemNA74xz7u2OqyBfaBgiydWgCU0QXhSkclnJjXunrB4IP~9C9P39ST2kre5WkJWwVg__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4"
         />
         <div className="icons">
-          <img className="heart" src="../../public/images/heart small.png" />
-          <img className="quick-view" src="../../public/images/Groop.png" />
+          <img className="heart" src={heartImage} alt="heart"/>
+          <img className="quick-view" src={groupImage} alt="group" />
         </div>
       </div>
 
